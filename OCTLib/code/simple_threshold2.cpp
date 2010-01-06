232e4ecffde4237e12d951250ff864bf0cebde49
rosmir <rosmir@8a1047ba-f9a2-11de-973d-3ba8dba7af4f>
2010-01-06 05:06:20 +0000

    add comments for threshold functions. update corresponding LabView code.

--- OCTLib/code/simple_threshold2.cpp
+++ OCTLib/code/simple_threshold2.cpp
@@ -17,7 +17,31 @@ extern "C" {
 DllExport I8 OL_simple_threshold2(U32, DBL, DBL, DBL, DBL, DBL *, DBL *);
 }
 
-// simple Threshold2 main function
+/* simple threshold #2 main function
+  PURPOSE:
+    make data filtering based on reference. Value from data array is kept if the
+    corresponding value from reference array is inside its defined range
+    (min...max), otherwise values from data are changed to new_max for all
+    corresponding values from reference array that are above max value, and
+    values from data are changed to new_min for all corresponding values from
+    reference that are below min value.
+  
+  INPUTS:
+    SIZE - number of elements in buffer (B-scan)
+    min - minimal intensity value to check
+    max - maximum intensity value to check
+    new_min - minimal phase value to assigned
+    new_max - maximum phase value to assigned
+    ref - pointer to buffer with intensity B-scan after FFT (size: SIZE)
+  
+  OUTPUTS:
+    out - pointer to buffer with filtered Doppler OCT or phase B-scan after FFT
+    (size: SIZE)
+  
+  REMARKS:
+    note this function is useful to filter Doppler OCT results or phase OCT
+    B-scans based on information from structural, intensity OCT B-scans.
+*/
 I8 OL_simple_threshold2(U32 SIZE, DBL min, DBL max, DBL new_min, DBL new_max,
                         DBL *ref, DBL *ptr) {
   I32 i;
