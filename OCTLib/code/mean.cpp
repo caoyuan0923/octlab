a836611b67e50e708e5701ccd1a709a67d192b83
rosmir <rosmir@8a1047ba-f9a2-11de-973d-3ba8dba7af4f>
2010-01-07 05:39:00 +0000

    update mean function

--- OCTLib/code/mean.cpp
+++ OCTLib/code/mean.cpp
@@ -14,19 +14,45 @@
 
 // for DLL export
 extern "C" {
-DllExport DBL OL_mean(U32, DBL *);
-DllExport I8  OL_mean_map(U32, U32, U32, DBL *, DBL *);
+DllExport I8  OL_mean_map(U32, U32, U32, U32, DBL *, DBL *);
 }
 
-// mean main function
-DBL OL_mean(U32 SIZE, DBL *ptr) {
-  return accumulate(ptr, ptr + SIZE, 0.0) / SIZE;
-}
-DBL (*fp_mean)(U32, DBL *) = OL_mean;
-
-/************OL_mean_map************/
-I8 OL_mean_map(U32 X, U32 Y, U32 radius, DBL *lineIN, DBL *lineOUT) {
-  return rsm_frame(X, Y, radius, lineIN, lineOUT, fp_mean);
+/* OL_mean_map main function
+  PURPOSE:
+    calculate mean function [1] for spatially sliding 2D window within B-scan.
+  
+  INPUTS:
+    X - number of elements in each row (A-scan size)
+    Y - number of rows (# of A-scans)
+    x_r - horizontal radius, defines width of 2D sliding window (2 * x_r + 1)
+    y_r - vertical radius, defines height of 2D sliding window (2 * y_r + 1)
+    in - pointer to buffer with B-scan after FFT (size: X * Y)
+  
+  OUTPUTS:
+    out - pointer to buffer with results (size: (X - 2 * x_r) * (Y - 2 * y_r))
+  
+  REFERENCES:
+    [1] http://en.wikipedia.org/wiki/Mean
+*/
+I8 OL_mean_map(U32 X, U32 Y, U32 x_r, U32 y_r, DBL *in, DBL *out) {
+  U32 d = X - 2 * x_r, x_d = 2 * x_r + 1, y_d = 2 * y_r + 1, size = x_d * y_d;
+  I32 x, y;
+  // parallel run by elements
+  #pragma omp parallel for default(shared) private(x, y)
+  for (x = 0; x < static_cast<I32>(d); x++) {  // horizontal
+    for (y = 0; y < static_cast<I32>(Y - 2 * y_r); y++) {  // vertical
+      DBL tmp = 0.0;
+      for (U32 i = x; i < x_d + x; i++) {
+        for (U32 j = 0, pos = y * X + i; j < y_d; j++, pos = pos + X) {
+          // sum
+          tmp = tmp + in[pos];
+        }
+      }
+      // fill out
+      out[y * d + x] = tmp / size;
+    }
+  }  // end of parallel code
+  return EXIT_SUCCESS;
 }
 
 /*******************************************************************************
